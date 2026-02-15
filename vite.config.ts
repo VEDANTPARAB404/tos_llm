4b5f9c3b67e4f8f1e55296babfdea55b65a664cb
Ved <76614076+VEDANTPARAB404@users.noreply.github.com>
2026-02-15 22:13:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-  // Load env file from current directory
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    define: {
-      // This injects the API_KEY from your .env file into the code
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
-    server: {
-      port: 3000,
-      open: true
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
+})
